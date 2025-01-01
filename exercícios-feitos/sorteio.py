7e80f8b2d7e4b251e964f3c928bd45c3c734132d
Wesley Medeiros <wescarmed@outlook.com>
2025-01-01 17:08:27 -0300

    Update sorteio.py

--- exercícios-feitos/sorteio.py
+++ exercícios-feitos/sorteio.py
@@ -4,4 +4,4 @@ import string
 # Gera uma letra aleatória de a a z
 letra_sorteada = random.choice(string.ascii_lowercase)
 
-print(f"A letra sorteada é: {letra_sorteada}")
+print(f"A letra sorteada é: {letra_sorteada}");
